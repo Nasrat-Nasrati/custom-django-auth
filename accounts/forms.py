b474571e0054c99a382e562fd9c3ef10250d513d
Nasrat Nasrati <115137645+Nasrat-Nasrati@users.noreply.github.com>
2025-07-11 11:24:33 +0430

    modified the style of login and signput form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,41 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 class CustomSignupForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your name', 'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email', 'class': 'form-control'})
+    )
+    phone = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your phone', 'class': 'form-control'})
+    )
+    address = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Enter your address', 'class': 'form-control'})
+    )
+    profile_image = forms.ImageField(
+        required=False,
+        widget=forms.ClearableFileInput(attrs={'class': 'form-control'})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Create password', 'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password', 'class': 'form-control'})
+    )
+
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'email', 'phone', 'address', 'profile_image', 'password1', 'password2']
 
+
 class CustomLoginForm(AuthenticationForm):
-    username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    username = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email', 'class': 'form-control'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password', 'class': 'form-control'})
+    )
